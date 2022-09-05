56847b4b209c8d53f48ce5e8bcbf061176fe1e5c
Dreamclaw <udvardifrancisco@gmail.com>
2022-09-05 18:01:34 -0300

    Add files via upload

--- clawDept.js
+++ clawDept.js
@@ -4,10 +4,31 @@ $(document).ready(function() {
     $( "#instro" ).delay(700).effect( "blind", 700 );
 })
 
+/* PLAY INTRO DIV */
+
+let introMouseDiv = document.getElementById("playIntro");
+let introMouseCont = document.getElementById("introButtCont");
+
+introMouseCont.onmouseenter = function() {
+    $(introMouseDiv).toggle( "clip", 500 );
+}
+introMouseCont.onmouseleave = function() {
+    $(introMouseDiv).toggle( "clip", 500 );
+}
+
+const onMouseMove = (e) =>{
+    const mouseY = e.pageY;
+    const mouseX = e.pageX;
+    introMouseDiv.style.top = mouseY + 'px'
+    introMouseDiv.style.left = mouseX + 'px'
+  }
+document.addEventListener('mousemove', onMouseMove);
+
+
 /* CHANGING INTRO BACKGROUND */
 
-var bckgImgs = ["sneakers.jpg","girl.jpg"];
-var introImgs = ["manWom.jpg","squat.jpg"];
+var bckgImgs = ["sneakers.jpg","girl.jpg","squat.jpg"];
+var introImgs = ["strength.jpg","style.jpg"];
 $(function () {
     var i = 0;
     var y = 0;
@@ -35,8 +56,10 @@ $(function () {
 });
 
 /* CAROUSEL TIME */
-let sliderContainer = document.querySelector('#carousel');
+let sliderContainer = document.querySelector('#carouselCont');
 let innerSlider = document.querySelector('#selUl');
+let dragButton = document.getElementById("pDrag");
+let dragText = document.getElementById("draggy");
 
 let pressed = false;
 let startX;
@@ -45,16 +68,16 @@ let x;
 sliderContainer.addEventListener('mousedown', (e) => {
     pressed = true;
     startX = e.offsetX - innerSlider.offsetLeft;
-    sliderContainer.style.cursor = "grabbing";
+    dragButton.style.width = "80px";
+    dragButton.style.height = "80px";
+    dragText.innerHTML = "";
 })
 
-sliderContainer.addEventListener("mouseenter", () => {
-    sliderContainer.style.cursor = "grab";
-});
-
-sliderContainer.addEventListener("mouseup", () => {
-    sliderContainer.style.cursor = "grab";
+sliderContainer.addEventListener("mouseup", () => {;
     pressed = false;
+    dragButton.style.width = "110px";
+    dragButton.style.height = "110px";
+    dragText.innerHTML = "DRAG";
 });
 
 sliderContainer.addEventListener("mousemove", (e) => {
@@ -81,6 +104,28 @@ const checkBoundary = () => {
     }
 };
 
+
+/* DRAG SLIDER BUTTON */
+
+let dragMouseDiv = document.getElementById("dragTop");
+let dragMouseCont = document.getElementById("carouselCont");
+
+dragMouseCont.onmouseenter = function() {
+    $(dragMouseDiv).toggle( "blind", 500 );
+}
+dragMouseCont.onmouseleave = function() {
+    $(dragMouseDiv).toggle( "blind", 500 );
+}
+
+const onDragMove = (e) =>{
+    const mouseY = e.pageY;
+    const mouseX = e.pageX;
+    dragMouseDiv.style.top = mouseY + 'px'
+    dragMouseDiv.style.left = mouseX + 'px'
+  }
+document.addEventListener('mousemove', onDragMove);
+
+
 /* STYLE CHANGING ON SCROLL */
 
 let elemY = $("#sect5");
@@ -89,7 +134,7 @@ let elemBot = elemY.position().top + elemY.outerHeight(true)
 
 $(function() {
     $(window).scroll(function () {
-       if ($(this).scrollTop() > elemTop - 400) {
+       if ($(this).scrollTop() > elemTop - 600) {
           $("body").addClass("blackyBckg")
           $(".4lee").addClass("pinkyf")
           $(".linky").addClass("pinkyf")
@@ -101,7 +146,7 @@ $(function() {
           $(".s6li").addClass("pinkyBord")
           $("hr").addClass("pinkyBord")
        } 
-       if ($(this).scrollTop() < elemTop - 400 || $(this).scrollTop() > elemBot - 200) {
+       if ($(this).scrollTop() < elemTop - 600 || $(this).scrollTop() > elemBot - 200) {
             $("body").removeClass("blackyBckg")
           $(".4lee").removeClass("pinkyf")
           $(".linky").removeClass("pinkyf")
